ab5e8ad751ee7fd0768bcddc7c3dff61a033e6ea
anbub <yangbn1906@gmail.com>
2026-04-24 23:13:38 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -637,7 +637,7 @@ def main():
     ui.render_cache_info()
     
     if need_fetch:
-        with st.spinner(f"📡 Loading data  {len(pages)} page ..."):
+        with st.spinner(f"📡 Loading Data {len(pages)} Fanpage ..."):
             pages_data = []
             progress = st.progress(0)
             
